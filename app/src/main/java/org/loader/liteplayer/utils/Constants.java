9e59035fb3bc67dcc6f68f7218920a135a232fde
andysunzhixiang <sunzhixiang.dalian@gmail.com>
2015-11-09 11:23:34 +0900

    delete

--- app/src/main/java/org/loader/liteplayer/utils/Constants.java
+++
@@ -1,9 +0,0 @@
-package org.loader.liteplayer.utils;
-
-public class Constants {
-	public static final String SP_NAME = "liteplayer";
-	public static final String PLAY_POS = "playing_position";
-	public static final String MUSIC_URL = "http://music.baidu.com";
-	public static final int SUCCESS = 1;
-	public static final int FAILED = 2;
-}
--- gen/org/loader/liteplayer/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.loader.liteplayer;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
